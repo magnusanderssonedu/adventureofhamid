9cc5ee5174ad6e77afbd517c37808fe45b836879
Magnus Andersson <bredsjomagnus@yahoo.se>
2019-05-06 13:47:34 +0200

    wider and higher player moves

--- player.py
+++ player.py
@@ -7,10 +7,10 @@ class Player:
     y = 0
     relx = 0
     rely = 0
-    width = 34 #width and height of player image on screen
-    height = 34
-    velx = 35  #width+borderwidth
-    vely = 35  #height+borderheight
+    width = 34 #width and height of player image on screen (was originally 34)
+    height = 34 # (was originally 34)
+    velx = 140  #width+borderwidth (was originally 35)
+    vely = 140  #height+borderheight (was originally 35)
     me = pygame.image.load(os.path.join('data', 'player.png'))
 
     def __init__(self,coords=(0,0)):
@@ -21,9 +21,24 @@ class Player:
     def move(self,movecords):
         """Sätter relx, rely (koordinaterna) samt x, y som är pixlarna"""
         self.relx = self.relx + movecords[0]
-        self.x = self.velx*self.relx
         self.rely = self.rely + movecords[1]
-        self.y = self.vely*self.rely
+        if self.relx == 0:
+            self.relx = self.relx + movecords[0]
+            self.x = 35
+        else:
+            # self.relx = self.relx + movecords[0]
+            self.x = self.velx*self.relx
+
+        if self.rely == 0:
+            self.y = 35
+        else:
+            self.y = self.vely*self.rely
+        
+        # self.relx = self.relx + movecords[0]
+        # self.x = self.velx*self.relx
+        # self.rely = self.rely + movecords[1]
+        # self.y = self.vely*self.rely
+        print("relx: {}, x: {}".format(self.relx, self.x))
 
     def draw(self):
         return self.me
