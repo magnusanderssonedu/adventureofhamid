5bb1ac05b9e9adb1d82e82602f5d8d2bef4653fa
PihlFredrik <fredrik.fogelqvist@pihlskolan.hellefors.se>
2019-05-07 20:50:19 +0200

    Mob class done. Updated zombie sprite. Mine and trap available.

--- mob.py
+++ mob.py
@@ -1,14 +1,64 @@
 import pygame
 import os
 
+"""This class is a quite generalized class for any type of obstacle, both active and inactiveself.
+Eg. a landmine aswell as a zombie is regarded as a mob in this context.
+"""
+
 class Mob:
     #Mob variables
     hp = 20 #mob health points
-    agressive = True #is the mob an attacking or passive Mob
+    agressive = True #is the mob an attacking or passive Mob. Passive mob being a trap door or mine.
     attack = 2 #damage dealt on attack. For passive mobs, like mines or trap doors, this will happen on trigger
-    trigger = 0.2 #percentage that the mob will attack
+    attacktrigger = 0.3 #percentage that the mob will return the attack.
+    fleetrigger = 0.2 #percentage that the mob will attack on flight. Usually a value lower than attack trigger unless the mob is passive
     description = "a mob" #Description for the status bar
     me = None #image of the mob
+    picpicker = {
+        "mine": "mob_mine.png",
+        "trap": "mob_trap.png",
+        "zombie": "mob_zombie.png"
+    }
 
-    def __init__():
-        self.relx = 0
+    def __init__(self,hp=0,description="Mine",aggressive=False,attack=10,attacktrigger=0.3,fleetrigger=0.2,pic="mine"):
+        self.hp = hp
+        self.aggressive = aggressive
+        self.attacktrigger = attacktrigger
+        self.fleetrigger = fleetrigger
+        self.trigger = trigger
+        self.description = description
+        self.me = pygame.image.load(os.path.join('data', self.picpicker[pic]));
+
+    def attack(self,dievalue,flee=False):
+        """
+        The value dievalue is a percentage and is obligatory
+        The value flee is a boolean representing whether the player is fleeing or not.
+        Returns a tuple with (hit,damage) where hit is either True or False and damage is the damage dealt by the mob on hit
+        """
+        retValue = (False,0)
+        if(flee):
+            triggering = self.fleetrigger
+        else:
+            triggering = self.attacktrigger
+
+        if(triggering > dievalue):
+            retValue = (True,self.attack)
+        return retValue
+
+    def hit(self,hit):
+        """
+        Mob is hit with 100% chance. Damage dealt is managed by player class.
+        This method sets hp after hit. Use getHPvalue() to get HP.
+        """
+        self.hp -= hit
+        if self.hp < 0:
+            self.hp = 0
+
+    def draw(self): #return image of mob for drawing
+        return self.me
+
+    def getDescription(self):   #return description for drawing on status
+        return self.description
+
+    def getHPvalue(self):  #return hp for drawing on status
+        return self.hp
