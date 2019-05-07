fd3db1ba71ab0691e53642acf6272d817e7a6529
PihlFredrik <fredrik.fogelqvist@pihlskolan.hellefors.se>
2019-05-07 21:05:54 +0200

    Added "killable" switch to make mobs that will return.

--- mob.py
+++ mob.py
@@ -8,26 +8,29 @@ Eg. a landmine aswell as a zombie is regarded as a mob in this context.
 class Mob:
     #Mob variables
     hp = 20 #mob health points
+    killable = True #if killable is False the mob will resurrect on every new encounter. Useful on traps that can be triggered multiple times.
     agressive = True #is the mob an attacking or passive Mob. Passive mob being a trap door or mine.
     attack = 2 #damage dealt on attack. For passive mobs, like mines or trap doors, this will happen on trigger
     attacktrigger = 0.3 #percentage that the mob will return the attack.
     fleetrigger = 0.2 #percentage that the mob will attack on flight. Usually a value lower than attack trigger unless the mob is passive
     description = "a mob" #Description for the status bar
-    me = None #image of the mob
+    me = None #image of the mob. Use keywords from picpicker to choose sprite
+
     picpicker = {
         "mine": "mob_mine.png",
         "trap": "mob_trap.png",
         "zombie": "mob_zombie.png"
     }
 
-    def __init__(self,hp=0,description="Mine",aggressive=False,attack=10,attacktrigger=0.3,fleetrigger=0.2,pic="mine"):
+    def __init__(self,hp=0,killable = True,description="Mine",aggressive=False,attack=10,attacktrigger=0.3,fleetrigger=0.2,sprite="mine"):
         self.hp = hp
+        self.killable = killable
         self.aggressive = aggressive
         self.attacktrigger = attacktrigger
         self.fleetrigger = fleetrigger
         self.trigger = trigger
         self.description = description
-        self.me = pygame.image.load(os.path.join('data', self.picpicker[pic]));
+        self.me = pygame.image.load(os.path.join('data', self.picpicker[sprite]));
 
     def attack(self,dievalue,flee=False):
         """
